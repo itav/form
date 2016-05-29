a80f1443df347f7c1db1a767e77c9aa37d32b8f7
Sylester Kowalski <sylwester7799@gmail.com>
2016-05-29 16:31:56 +0200

    render textarea

--- src/views/textarea.php
+++ src/views/textarea.php
@@ -5,5 +5,5 @@
         <?php if (isset($data['name'])): ?> name="<?php echo $data['name'] ?>" <?php endif; ?> 
         <?php if (isset($data['type'])): ?> type="<?php echo $data['type'] ?>" <?php endif; ?>
         <?php if (isset($data['value'])): ?> value="<?php echo $data['value'] ?>" <?php endif; ?>>
-    <?php if (isset($data['value'])): ?> value="<?php echo $data['value'] ?>" <?php endif; ?>
+    <?php if (isset($data['value'])): ?><?php echo $data['value'] ?><?php endif; ?>
 </textarea>
